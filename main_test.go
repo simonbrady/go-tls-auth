319850686cae986a74df228f884484c5883a9454
Simon Brady <simon@hikari.org.nz>
2023-05-22 00:01:39 +1200

    Increase test coverage

--- main_test.go
+++ main_test.go
@@ -1,12 +1,23 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+const want = "CN=gopher"
+const addr = "127.0.0.1:12345"
+
+func dummyPassword() []byte {
+	return []byte("password")
+}
 
 func TestLoadCertificate(t *testing.T) {
-	const want = "CN=gopher"
-	getPassword = func() []byte {
-		return []byte("password")
-	}
+	getPassword = dummyPassword
 	for _, filename := range []string{
 		"testdata/certfirst.pem",
 		"testdata/certfirst_decrypted.pem",
@@ -21,5 +32,62 @@ func TestLoadCertificate(t *testing.T) {
 		if got != want {
 			t.Errorf("failed on %s: got subject %q, want %q", filename, got, want)
 		}
+	}
+}
+
+type handler struct {
+	Shutdown context.CancelFunc
+	Test     *testing.T
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	defer h.Shutdown()
+	t := h.Test
+	certs := len(req.TLS.PeerCertificates)
+	if certs != 1 {
+		t.Errorf("got %d client certificates, expected 1", certs)
+		return
+	}
+	got := req.TLS.PeerCertificates[0].Subject.String()
+	if got != want {
+		t.Errorf("got subject %q, want %q", got, want)
+		return
+	}
+}
+
+func startServer(server http.Server, t *testing.T) {
+	if err := server.ListenAndServeTLS("testdata/test.crt", "testdata/decrypted.key"); err != http.ErrServerClosed {
+		t.Error(err)
+	}
+}
+
+func TestClient(t *testing.T) {
+	die = func(err error) {
+		t.Error(err)
+	}
+	getPassword = dummyPassword
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server := http.Server{
+		Addr: addr,
+		Handler: handler{
+			Shutdown: cancel,
+			Test:     t,
+		},
+		TLSConfig: &tls.Config{
+			ClientAuth: tls.RequestClientCert,
+		},
+	}
+	go startServer(server, t)
+	cert := loadCert("testdata/certfirst.pem")
+	client := getClient(cert)
+	resp := must(client.Get(fmt.Sprintf("https://%s", addr)))
+	defer resp.Body.Close()
+	<-ctx.Done()
+	if ctx.Err() != context.Canceled {
+		t.Errorf("server timed out waiting for client")
+	}
+	if err := server.Shutdown(context.Background()); err != nil {
+		t.Error(err)
 	}
 }
